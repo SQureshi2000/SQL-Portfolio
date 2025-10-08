b9b6c659adb5dd5119a322f90fa680d6440bd018
SQureshi2000 <shahir.qureshi0@gmail.com>
2025-10-08 14:15:41 +0100

    Update queries.sql

--- Week1_CustomerOrders/queries.sql
+++ Week1_CustomerOrders/queries.sql
@@ -4,3 +4,10 @@ FROM Orders
 ORDER BY Amount DESC
 LIMIT 2;
 
+-- Drill 2: Show the 2 most recent orders by Order_Date
+SELECT Order_ID, Order_Date
+FROM Orders
+ORDER BY Order_Date DESC
+LIMIT 2;
+
+
